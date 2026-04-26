046dec6d6e0cb9a61cb69f1c0e716fa4c46b9f9f
Dung160405 <duongtridung1604@gmail.com>
2026-04-27 01:01:48 +0700

    add: choose prefab in tool editor

--- Assets/Scripts/ToolEditor/GridMapEditorWindow.cs
+++ Assets/Scripts/ToolEditor/GridMapEditorWindow.cs
@@ -1,17 +1,33 @@
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEditor;
 using UnityEngine;
 
 public class GridMapEditorWindow : EditorWindow
 {
     // --- CẤU HÌNH ---
-    private float gridSize = 1f;
-    private int selectedBlockIndex = 0;
+    private Vector2Int gridSize = new Vector2Int(1, 1);
 
-    private Dictionary<Vector3Int, GameObject> mapBlocks;
+    private Vector3 originPosition = Vector3.zero;
     
-    // Giả lập danh sách các block. Sau này ta sẽ thay bằng danh sách Prefab thật.
-    private string[] blockNames = { "Đất (Dirt)", "Đá (Stone)", "Gỗ (Wood)" };
+    private bool isMapCreated = false;
+
+    // Data for brick
+
+    private string brickFolderPath = "Assets/Prefabs/MapBricks";
+    private BrickBase[] availableBrick;
+
+    private string[] brickNames;
+    private int selectedBrickIndex = 0;
+
+    // Cache data
+
+    private List<Pair<Vector2Int, BlockState>> mapBricks;
+
+    private Dictionary<Vector2Int , bool> placedBrickDict;
+
+    private Transform root;
+    
 
     //Create menu item on Unity
     [MenuItem("Tools/Map Designer")]
@@ -27,10 +43,73 @@ public class GridMapEditorWindow : EditorWindow
     {
 
 
-        GUILayout.Label("EDIT SIZE OF A ZONE (Square Size)");
+        GUILayout.Label("EDIT SIZE OF MAP (Square Size)");
 
-        gridSize = EditorGUILayout.Slider("Size:", gridSize, 7f, 30f);
-        
+        gridSize.x = EditorGUILayout.IntField("Size X:", gridSize.x);
+        gridSize.y = EditorGUILayout.IntField("Size Y:", gridSize.y);
+
+        EditorGUILayout.Space(10);
+        if (GUILayout.Button("Create Map"))
+        {
+            //Draw map again
+            isMapCreated = true;
+            SceneView.RepaintAll();
+        }
+
+        EditorGUILayout.Space(10);
+
+        GUILayout.Label("CHỌN BRICK");
+
+        if(brickNames != null && brickNames.Length > 0)
+        {
+            selectedBrickIndex = EditorGUILayout.Popup("Choosing block: ", selectedBrickIndex, brickNames);
+        }
+
+        if (GUILayout.Button("Làm mới danh sách Gạch"))
+        {
+            LoadBricksFromAsset();
+        }
+
+    }
+
+    private void LoadBricksFromAsset()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:Prefab", new[] {brickFolderPath});
+        List<BrickBase> validBricks = new List<BrickBase>();
+
+        foreach(string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+
+            GameObject prefabAsset = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            if (prefabAsset != null)
+            {
+                BrickBase brickComponent = prefabAsset.GetComponent<BrickBase>();
+                if (brickComponent != null) validBricks.Add(brickComponent);
+            }
+        }
+        availableBrick = validBricks.ToArray();
+        brickNames = new string[availableBrick.Length];
+        for(int i = 0; i < availableBrick.Length; i++)
+        {
+            brickNames[i] = availableBrick[i].gameObject.name;
+        }
+    }
+
+    private void CreateMap()
+    {
+        Handles.color = new Color(0f, 1f, 1f, 0.8f);
+        //Draw Grid map by draw x line horizontal and y line vertical
+
+        for(int row = 0; row <= gridSize.x; row++)
+        {
+            Handles.DrawLine(new Vector3(-row, 0f, 0f), new Vector3(-row, 0f, -gridSize.y));
+        }
+
+        for(int collumn = 0; collumn <= gridSize.y; collumn++)
+        {
+            Handles.DrawLine(new Vector3(0, 0f, -collumn), new Vector3(-gridSize.x, 0f, -collumn));
+        }
     }
 
     // Subcribe event to draw on Scene view
@@ -47,8 +126,26 @@ public class GridMapEditorWindow : EditorWindow
     // Nơi bạn xử lý thao tác click chuột và vẽ lưới trên Scene 3D
     private void OnSceneGUI(SceneView sceneView)
     {
-        // Code xử lý Raycast chuột và vẽ Object mờ ở đây...
+        //Draw map
+        if (isMapCreated)
+        {
+            CreateMap();
+        } 
 
-        
+             
+    }
+
+
+}
+
+public struct Pair<T1, T2>
+{
+    public T1 First;
+    public T2 Second;
+
+    public Pair(T1 first, T2 second)
+    {
+        First = first;
+        Second = second;
     }
 }
--- Assets/Scripts/Ultility/GridHelper.cs
+++ Assets/Scripts/Ultility/GridHelper.cs
@@ -1,14 +1,22 @@
-using System.Numerics;
+
+using UnityEngine;
 
 public class GridHelper
 {
     public Vector3 ConvertGridToWorldPosition(int x, int z, Vector3 originPos)
     {
         
-        Vector2 cellSize = new Vector2(1f, 1f);
-        Vector3 worldPos = new Vector3(originPos.X - x * GameConfig.CellSize.x - GameConfig.CellSize.x / 2f, 0f, originPos.Z - z * GameConfig.CellSize.y - GameConfig.CellSize.y / 2f);
+        Vector3 worldPos = new Vector3(originPos.x - x * GameConfig.CellSize.x - GameConfig.CellSize.x / 2f, 0f, originPos.z - z * GameConfig.CellSize.y - GameConfig.CellSize.y / 2f);
 
         return worldPos;
+    }
+
+    public Vector2Int ConvertWorldPositionToGridPosition(Vector3 worldPosition, Vector3 originPos)
+    {
+        Vector2Int gridPosition = new Vector2Int((Mathf.FloorToInt((worldPosition.x - originPos.x + GameConfig.CellSize.x / 2f)/(- GameConfig.CellSize.x))),
+        Mathf.FloorToInt((worldPosition.z - originPos.z + GameConfig.CellSize.y / 2f)/ (-GameConfig.CellSize.y)));
+
+        return gridPosition;
     }
 
 }
